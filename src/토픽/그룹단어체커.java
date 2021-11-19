58be00152346411abeaa7527a3430f8dff32864b
노경 <@DESKTOP-G1O49A3>
2021-11-19 16:39:42 +0900

    message

--- src/토픽/그룹단어체커.java
+++ src/토픽/그룹단어체커.java
@@ -1,28 +1,33 @@
 package 토픽;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 public class 그룹단어체커 {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		String st = new String();
- 		int[] al = new int[25];
-		int cnt = n;
- 		Arrays.fill(al, 0);
- 		
-		for (int i = 0 ; i <n ; i++) {
-			st = br.readLine();
-		}
-		for(int i = 1 ; i < st.length() ; i++) {
-			if(st.charAt(i) != st.charAt(i-1)) {
-				cnt --;
-				break;
-			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int num= Integer.parseInt(br.readLine());
+			int cnt=num;
+
+			for(int c = 0;c<num;c++) {
+				String st = br.readLine();
+				boolean test[] = new boolean[26];
+
+				for(int i=0;i<st.length()-1;i++) {
+					
+					if(st.charAt(i)!=st.charAt(i+1)) {
+						if(test[st.charAt(i+1)-97]==true) {
+							cnt--;
+							break;
+						}
+					}
+			
+					test[st.charAt(i)-97]=true;
+				}
 				
-		}
+			}
+			
 			System.out.println(cnt);
-	
+		}
 	}
-}
+
