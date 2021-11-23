da91c35ff953c26aef718ee173ff893d59900d34
노경 <@DESKTOP-G1O49A3>
2021-11-23 21:24:40 +0900

    message

--- src/토픽/분수찾기.java
+++ src/토픽/분수찾기.java
@@ -4,29 +4,20 @@ public class 분수찾기 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int line = 1;
-		int cnt = 1;
-		int top = 0;
-		int bot = 0;
-		
-		if(n==1) {
-			System.out.printf("%d/%d",1, 1);
-		}
-		else {
-			while(cnt>=n) {
-				if(line % 2 ==0) {
-				top = line - line;
-				top++;
-				bot = line+1;
-				line--;
-				cnt ++;
+		int a = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		int n = 0;
+		while (true) {
+			n++;
+			cnt +=n;
+			if(cnt>=a) {
+				if(n%2==0) {
+					System.out.println(a-cnt+n+"/"+(cnt-a+1));
+				}else {
+					System.out.println((cnt-a+1)+"/"+(a-cnt+n));
 				}
+				break;
 			}
 		}
-		
-		
-		
 	}
-
 }
