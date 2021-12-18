6f8c58bc61d393580f467e3ed23050e9252ced56
Parknogyung <gerie99@naver.com>
2021-12-18 16:58:42 +0900

    me

--- src/토픽/셀프넘버.java
+++ src/토픽/셀프넘버.java
@@ -1,31 +1,20 @@
 package 토픽;
 
+import java.util.Arrays;
+
 public class 셀프넘버 {
 
 	public static void main(String[] args) {
-boolean[] isNotSelfNumber = new boolean[10001];
+		boolean [] f = new boolean[10001];
+		Arrays.fill(f, false);
 		
-		for (int i = 1; i <= 10000; ++i) {
-			int dn = getDn(i);
-			if (dn <= 10000) {
-				isNotSelfNumber[dn] = true;				
+		for(int i = 1 ; i < 10001 ; i ++) {
+			if(i==1 && i==3 && i==5 && i==7 && i==9) {
+			f[i]= true;
+			}		
+			else if (i>=10) {
+				if()
 			}
-		}
 		
-		for (int i = 1; i < isNotSelfNumber.length; ++i) {
-			if (!isNotSelfNumber[i]) {
-				System.out.println(i);				
-			}
-		}
 	}
-	
-	private static int getDn(int n) {
-		int dn = n;
-		while(n > 0) {
-			dn += n % 10;
-			n /= 10;
-		}
-		return dn;
-	}
-
 }
